83f256633a8d9934ae1283d2e6c980c8e1bb61d8
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-04-02 18:44:21 -0400

    nanoid package added

--- src/components/dieComponents/Die.js
+++ src/components/dieComponents/Die.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container } from 'react-bootstrap/Container';
 
 export default function Die() {
 
   return (
     <div>
-
+      <Container />
     </div>
   )
 }
