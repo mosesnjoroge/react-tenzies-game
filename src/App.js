509ed14f0d1fc10b7166947d1286b35fcdcc3803
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-04-02 19:35:07 -0400

    component generation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './components/css/App.css';
+import Header from './components/dieComponents/Header';
+import NewDie from './components/dieComponents/New Die';
+import Die from './components/dieComponents/Die';
 
 function App() {
+
+
   return (
     <div className="App">
-
+      <Header />
+      <Die />
+      <NewDie />
     </div>
   );
 }
