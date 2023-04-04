c28979fd1d5fb8b19d73f6ff1a55cd9faeb9908d
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-04-04 19:58:43 -0400

    boostrap package updated

--- src/components/dieComponents/Newdie.js
+++ src/components/dieComponents/Newdie.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { Button } from 'react-bootstrap/Button';
 
 export default function Newdie() {
 
   return (
     <div>
-
+      <Button variant="primary">Roll</Button>
     </div>
   )
 }
