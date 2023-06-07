ba7052d99d5c1c0bf6a516d312ac50940f16a6c6
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-06-07 19:20:50 -0400

    removal of commented code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Die from './components/dieComponents/Die';
 import { nanoid } from 'nanoid';
 import {React, useState, useEffect} from 'react';
 import Confetti from 'react-confetti';
-// import { Modal, Button } from 'react-bootstrap';
 
 
 function App() {
